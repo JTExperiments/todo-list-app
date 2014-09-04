4df743abc2f246e32ba891e084f5969c57863ce1
James Tang <j@jamztang.com>
2014-09-04 23:07:22 +0800

    New Todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,17 @@ class TodosController < ApplicationController
 	def index
 	  @todos = Todo.all
 	end
+
+	def new
+	  @todo = Todo.new
+	end
+
+	def create
+	  @todo = Todo.create(todo_params)
+	  redirect_to todos_path
+	end
+	private
+	def todo_params
+	  params.require(:todo).permit(:item)end
+
 end
